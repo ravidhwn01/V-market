b70cbd53be0c26ee874ce170aee91c2266c7aad5
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-04-04 13:13:25 +0530

    create login component

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,10 +1,70 @@
-import { Heading, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
+import { FieldValues, useForm } from "react-hook-form";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onHandleSubmit = (loginUserDetail: FieldValues) => {
+    console.log(loginUserDetail);
+  };
+
   return (
     <>
-      <Heading>login page</Heading>
-      <Text> loading data ...</Text>
+      <Heading textAlign={"center"} m="3">
+        Login{" "}
+      </Heading>
+      <form onSubmit={handleSubmit(onHandleSubmit)}>
+        <Flex
+          justifyContent={"center"}
+          direction={"column"}
+          w={"50%"}
+          m={"auto"}
+        >
+          <FormControl isInvalid={!!errors["email"]?.message}>
+            <FormLabel my="1">Email</FormLabel>
+            <Input
+              type="email"
+              placeholder="Enter Your Email"
+              {...register("email")}
+            />
+            {errors["email"]?.message && (
+              <FormErrorMessage>
+                {" "}
+                {errors["email"].message as any}{" "}
+              </FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isInvalid={!!errors["password"]?.message}>
+            <FormLabel my="1">Password</FormLabel>
+            <Input
+              type="password"
+              placeholder="Enter Your Password"
+              {...register("password")}
+            />
+            {errors["password"]?.message && (
+              <FormErrorMessage>
+                {" "}
+                {errors["password"].message as any}{" "}
+              </FormErrorMessage>
+            )}
+          </FormControl>
+
+          <Button w="100%" mt={4} bg="#aac6ca" type="submit">
+            Submit
+          </Button>
+        </Flex>
+      </form>
     </>
   );
 }
