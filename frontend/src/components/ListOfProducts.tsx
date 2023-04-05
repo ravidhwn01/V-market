1893d5ad71af70b477c15215463756cc3bc9749a
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-04-06 00:01:17 +0530

    add product to export component

--- frontend/src/components/ListOfProducts.tsx
+++ frontend/src/components/ListOfProducts.tsx
@@ -1,18 +1,28 @@
 import { Button, Flex, Grid, GridItem, Image, Text } from "@chakra-ui/react";
 import _ from "lodash";
 
-import { useQuery } from "react-query";
-import { useParams } from "react-router";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useNavigate, useParams } from "react-router";
 import { getAllProduct } from "../api/product.api";
 import { Product } from "../interfaces/shop.interface";
 import AddProduct from "./AddProduct";
 import Navbar from "./Navbar";
+import { addProductsToExport } from "../api/trade.api";
 
 function ListOfProducts() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const { shopId } = useParams();
   const { data } = useQuery<Product[]>(`products-${shopId}`, () => {
     return getAllProduct(shopId!);
     // we are certain that shopId will not be null or undefined at this point.
+  });
+
+  const addProductToExport = useMutation(addProductsToExport, {
+    onSuccess: () => {
+      queryClient.refetchQueries(`products-${shopId}`);
+      navigate("/exportedproducts");
+    },
   });
 
   return (
@@ -26,7 +36,8 @@ function ListOfProducts() {
         m="8"
         justifyContent={"center"}
       >
-        {_.map(data, (product, index) => {
+        {_.map(data, (product) => {
+          const { id } = product;
           return (
             <GridItem
               p="6"
@@ -46,7 +57,19 @@ function ListOfProducts() {
                 <Button bg="none">
                   <span>Quantity: </span> {product.quantity}
                 </Button>
-                <Button bg={"#aac6ca"}> Export </Button>
+                <Button
+                  onClick={() =>
+                    addProductToExport.mutate({
+                      productId: id,
+                      exportedShopkeeperId: shopId,
+                      importedShopkeeper: false,
+                    })
+                  }
+                  bg={"#aac6ca"}
+                >
+                  {" "}
+                  Export{" "}
+                </Button>
               </Flex>
             </GridItem>
           );
